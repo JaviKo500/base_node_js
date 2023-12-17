1703ed9195cbbf71591627b03aa4610799a94182
JaViKo500 <javikogutierrez64@gmail.com>
2023-12-17 16:30:13 -0500

    test in file 01-template

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -2,6 +2,12 @@ import { describe } from "node:test";
 
 describe('App', () => {
     test('should be true', () => {
-        expect(true).toBe(true);
+        // 1. arrange
+        const num1 = 10;
+        const num2 = 20;
+        // 2. act
+        const result = num1 + num2;
+        // 3. assert
+        expect(result).toBe(30);
     });
 });
