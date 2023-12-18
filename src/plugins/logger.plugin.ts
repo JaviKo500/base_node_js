b1f59173a727416e2644bb97a093b10f24b3cf5d
JaViKo500 <javikogutierrez64@gmail.com>
2023-12-17 20:45:48 -0500

    test plugin logger

--- src/plugins/logger.plugin.ts
+++ src/plugins/logger.plugin.ts
@@ -5,7 +5,7 @@ import winston from 'winston';
 const { combine, timestamp, json } = winston.format;
 
 
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
     level: 'info',
     format: combine(
         timestamp(),
@@ -13,27 +13,27 @@ const logger = winston.createLogger({
     ),
     // defaultMeta: { service },
     transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
     ],
 });
 
 
 logger.add(new winston.transports.Console({
-  format: winston.format.simple(),
+    format: winston.format.simple(),
 }));
 
 export const buildLogger = (service: string) => {
     return {
-        log: ( message: string ) => {
-            logger.log( 'info', {message, service} );
+        log: (message: string) => {
+            logger.log('info', { message, service });
         },
-        error: ( message: string ) => {
-            logger.error( 'error', {message, service, })
+        error: (message: string) => {
+            logger.error('error', { message, service, })
         }
     }
 }
